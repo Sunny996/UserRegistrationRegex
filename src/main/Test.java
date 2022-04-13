a019de31a513d1f2b14f8a2058493085fba4feb7
Sunny996 <satlaakash@gmail.com>
2022-04-14 00:57:09 +0530

    ADDED: A regex to match password of min 8 characters with a cap & number & 1 special character

--- src/main/Test.java
+++ src/main/Test.java
@@ -9,7 +9,7 @@ public class Test {
         System.out.println(Pattern.matches(userRegistration.lastName, "Satla"));
         System.out.println(Pattern.matches(userRegistration.email, "akash.satla@gmail.co.in"));
         System.out.println(Pattern.matches(userRegistration.phoneNumber, "91 9676443328"));
-        System.out.println(Pattern.matches(userRegistration.password, "Ajv12jafvc"));
+        System.out.println(Pattern.matches(userRegistration.password, "Ajv12@jafvc"));
 
     }
 }
--- src/main/UserRegistration.java
+++ src/main/UserRegistration.java
@@ -5,6 +5,6 @@ public class UserRegistration {
     public String lastName = "^[A-Z]{1}[A-Za-z]{2}.*";
     public String email = "^[a-z0-9_+-]+((\\.)[a-z0-9]+)?(\\@)[a-z0-9]+(\\.)[a-z]{2,4}((\\.)[a-z]{2,4})?$";
     public String phoneNumber = "^[0-9]{1,3} [0-9]{10}$";
-    public String password = "^(?=.*[A-Z])(?=.*\\d)[a-zA-Z0-9]{8,}$";
+    public String password = "^(?=.*[A-Z])(?=.*\\d)(?=[a-zA-Z0-9]*[@$#%^&!?][a-zA-Z0-9]*$)[a-zA-Z0-9!@#$^&*]{8,}$";
 
 }
