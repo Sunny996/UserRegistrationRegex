5d908d9ec2050eb2e93e802d6e28c0f0e5a21f2e
Akash Satla <65150847+Sunny996@users.noreply.github.com>
2022-04-14 09:12:12 +0530

    Update UserRegistration.java

--- src/main/UserRegistration.java
+++ src/main/UserRegistration.java
@@ -1,9 +1,9 @@
 package main;
 
 public class UserRegistration {
-    public String firstName = "^[A-Z]{1}[A-Za-z]{2}.*";
-    public String lastName = "^[A-Z]{1}[A-Za-z]{2}.*";
+    public String firstName = "^[A-Z]{1}[a-z]{2}[a-z]*$";
+    public String lastName = "^[A-Z]{1}[a-z]{2}[a-z]*$";
     public String email = "^[a-z0-9_+-]+((\\.)[a-z0-9]+)?(@)[a-z0-9]+(\\.)[a-z]{2,4}(\\.)[a-z]{2,4}$";
-    public String phoneNumber = "^[0-9]{1,3} [0-9]{10}$";
+    public String phoneNumber = "^[1-9][0-9]{0,2} [1-9][0-9]{9}$";
 
 }
