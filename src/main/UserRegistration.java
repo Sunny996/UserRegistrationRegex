358ce82595a014b20ade3619206a63cce906a319
Akash Satla <65150847+Sunny996@users.noreply.github.com>
2022-04-14 09:17:17 +0530

    Update UserRegistration.java

--- src/main/UserRegistration.java
+++ src/main/UserRegistration.java
@@ -1,6 +1,6 @@
 package main;
 
 public class UserRegistration {
-    public String firstName = "^[A-Z]{1}[a-z]{2}[a-z]*$";
-    public String lastName = "^[A-Z]{1}[a-z]{2}[a-z]*$";
+    public String firstName = "^[A-Z][a-z]{2,}$";
+    public String lastName = "^[A-Z][a-z]{2,}$";
 }
